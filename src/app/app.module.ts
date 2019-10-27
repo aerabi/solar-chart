f08ec27b9ae02e0aac4daca8e34b43d904e7596e
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2019-10-27 22:05:53 +0100

    Add Bootstrap and header buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent
+    ChartComponent,
+    ButtonsComponent
   ],
   imports: [
+    AngularFontAwesomeModule,
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
