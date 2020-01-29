a8af2231defcfbcb1f011c63f753d8484e2ce8c5
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2020-01-29 22:41:45 +0100

    Use loaded data add a series for each data point

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, NgZone, OnDestroy} from '@angular/core';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
@@ -25,61 +25,77 @@ export class ChartComponent implements AfterViewInit, OnDestroy {
       const chart = am4core.create('chartdiv', am4charts.XYChart);
 
 // Add data
-      chart.data = this.loadData(DUMMY_JSON);
+      const loadedData = this.loadData(DUMMY_JSON);
+      chart.data = loadedData;
+      console.log(loadedData);
 
-// Set input format for the dates
-//       chart.dateFormatter.inputDateFormat = 'yyyy-MM-dd';
+      // Set input format for the dates
+      // chart.dateFormatter.inputDateFormat = 'yyyy-MM-dd';
       chart.dateFormatter.inputDateFormat = 'HHmm';
 
-// Create axes
+      // Create date axes
       const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+      dateAxis.renderer.minGridDistance = 50;
+      dateAxis.renderer.grid.template.disabled = true;
+      dateAxis.renderer.fullWidthTooltip = true;
+
+      // Create value axis
       const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.title.text = 'Voltage';
 
-// Create series
-      const series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.valueY = 'value';
-      series.dataFields.dateX = 'date';
-      series.tooltipText = '{value}';
-      series.strokeWidth = 2;
-      series.minBulletDistance = 15;
+      Object.keys(loadedData[0])
+        .filter(key => key !== 'time')
+        .forEach(key => {
+          console.log(`Creating series ${key}`);
 
-// Drop-shaped tooltips
-      series.tooltip.background.cornerRadius = 20;
-      series.tooltip.background.strokeOpacity = 0;
-      series.tooltip.pointerOrientation = 'vertical';
-      series.tooltip.label.minWidth = 40;
-      series.tooltip.label.minHeight = 40;
-      series.tooltip.label.textAlign = 'middle';
-      series.tooltip.label.textValign = 'middle';
+          // Create series
+          const series = chart.series.push(new am4charts.LineSeries());
+          series.dataFields.valueY = key;
+          series.dataFields.dateX = 'time';
+          series.tooltipText = '{value}';
+          series.strokeWidth = 2;
+          series.minBulletDistance = 15;
 
-// Make bullets grow on hover
-      const bullet = series.bullets.push(new am4charts.CircleBullet());
-      bullet.circle.strokeWidth = 2;
-      bullet.circle.radius = 4;
-      bullet.circle.fill = am4core.color('#fff');
+          // Drop-shaped tooltips
+          series.tooltip.background.cornerRadius = 20;
+          series.tooltip.background.strokeOpacity = 0;
+          series.tooltip.pointerOrientation = 'vertical';
+          series.tooltip.label.minWidth = 40;
+          series.tooltip.label.minHeight = 40;
+          series.tooltip.label.textAlign = 'middle';
+          series.tooltip.label.textValign = 'middle';
 
-      const bullethover = bullet.states.create('hover');
-      bullethover.properties.scale = 1.3;
+          // Make bullets grow on hover
+          const bullet = series.bullets.push(new am4charts.CircleBullet());
+          bullet.circle.strokeWidth = 2;
+          bullet.circle.radius = 4;
+          bullet.circle.fill = am4core.color('#fff');
 
-// Make a panning cursor
-      chart.cursor = new am4charts.XYCursor();
-      chart.cursor.behavior = 'panXY';
-      chart.cursor.xAxis = dateAxis;
-      chart.cursor.snapToSeries = series;
+          const bullethover = bullet.states.create('hover');
+          bullethover.properties.scale = 1.3;
 
-// Create vertical scrollbar and place it before the value axis
-      chart.scrollbarY = new am4core.Scrollbar();
-      chart.scrollbarY.parent = chart.leftAxesContainer;
-      chart.scrollbarY.toBack();
+          // Make a panning cursor
+          chart.cursor = new am4charts.XYCursor();
+          chart.cursor.behavior = 'panXY';
+          chart.cursor.xAxis = dateAxis;
+          chart.cursor.snapToSeries = series;
 
-// Create a horizontal scrollbar with previe and place it underneath the date axis
-      chart.scrollbarX = new am4charts.XYChartScrollbar();
-      // chart.scrollbarX.series.push(series);
-      chart.scrollbarX.parent = chart.bottomAxesContainer;
+          // Create vertical scrollbar and place it before the value axis
+          chart.scrollbarY = new am4core.Scrollbar();
+          chart.scrollbarY.parent = chart.leftAxesContainer;
+          chart.scrollbarY.toBack();
 
-      chart.events.on('ready', () => {
-        dateAxis.zoom({start: 0.79, end: 1});
-      });
+          // Create a horizontal scrollbar with previe and place it underneath the date axis
+          chart.scrollbarX = new am4charts.XYChartScrollbar();
+          // @ts-ignore
+          chart.scrollbarX.series.push(series);
+          chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+          chart.events.on('ready', () => {
+            dateAxis.zoom({start: 0.79, end: 1});
+          });
+        });
     });
   }
 
